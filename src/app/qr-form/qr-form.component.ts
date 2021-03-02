ed7eb9c92601290646ce6cb78fd8c2dd6ee6ce75
zbelicza85 <zoltan.belicza@vertican.com>
2021-03-02 17:05:34 +0100

    finishing basic form process, adding qr code generator to the project

--- src/app/qr-form/qr-form.component.ts
+++ src/app/qr-form/qr-form.component.ts
@@ -1,14 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import QRCode from "easyqrcodejs";
 
 @Component({
   selector: 'app-qr-form',
   templateUrl: './qr-form.component.html',
   styleUrls: ['./qr-form.component.css']
 })
-export class QrFormComponent implements OnInit {
+export class QrFormComponent implements OnInit, AfterViewInit {
+  @ViewChild('qrcode', {static: false}) qrcode!: ElementRef;
   onlyBasicVisible = true;
-  qrCodeSettings = {};
+  qrCodeSettings = <any>{};
   qrCodeForm!: FormGroup;
   codeSubmitted = false;
   codeErrors: number = 0;
@@ -21,10 +23,23 @@ export class QrFormComponent implements OnInit {
   ngOnInit(): void {
     this.qrCodeForm = this.fb.group(
       {
-        basicText: ['', [Validators.required, , Validators.minLength(4)]],
-      },
-      { updateOn: 'change' }
+        basicText: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(256)]],
+        basicWidth: ['256', [Validators.required, Validators.min(0), Validators.max(1024)]],
+        basicHeight: ['256', [Validators.min(0), Validators.max(1024)]],
+        basicEqualSizes: ['', []],
+        basicColorDark: ['#000000', [Validators.pattern('^#+([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$')]],
+        basicColorLight: ['#ffffff', [Validators.pattern('^#+([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$')]],
+        },  
+        { 
+          updateOn: 'change'
+        }
     );
+  }
+
+  ngAfterViewInit() {
+    var options = {
+      text: "https://github.com/ushelp/EasyQRCodeJS"
+    }
   }
 
   showMoreSettings(): void {
@@ -34,6 +49,21 @@ export class QrFormComponent implements OnInit {
   get basicText() {
     return this.qrCodeForm.get('basicText');
   }
+  get basicWidth() {
+    return this.qrCodeForm.get('basicWidth');
+  }
+  get basicHeight() {
+    return this.qrCodeForm.get('basicHeight');
+  }
+  get basicEqualSizes() {
+    return this.qrCodeForm.get('basicEqualSizes');
+  }
+  get basicColorDark() {
+    return this.qrCodeForm.get('basicColorDark');
+  }
+  get basicColorLight() {
+    return this.qrCodeForm.get('basicColorLight');
+  }
 
   resetForm(): void {
     this.showMoreSettings();
@@ -42,7 +72,43 @@ export class QrFormComponent implements OnInit {
   qrCodeSubmit() {
     let formValues;
     formValues = this.qrCodeForm.getRawValue();
-    console.dir(formValues);
-    // TODO: finish this
+
+    if (formValues.basicText) { // title check
+      if (formValues.basicText.length > 4 || formValues.basicText.length < 260) {
+        this.qrCodeSettings = { ...this.qrCodeSettings, text: formValues.basicText }
+      } else { this.codeErrors++ }
+    }
+
+    if (formValues.basicWidth) { // width check
+      if (formValues.basicWidth < 1024 || formValues.basicWidth > 0) {
+        this.qrCodeSettings = { ...this.qrCodeSettings, width: formValues.basicWidth }
+      } else { this.codeErrors++ }
+    }
+
+    if (formValues.basicEqualSizes) { // height and size check
+      if (formValues.basicWidth < 1024 || formValues.basicWidth > 0) {
+        this.qrCodeSettings = { ...this.qrCodeSettings, height: formValues.basicWidth }
+      } else { this.codeErrors++ }
+    } else {
+      if (formValues.basicHeight) {
+        if (formValues.basicHeight < 1024 || formValues.basicHeight > 0) {
+          this.qrCodeSettings = { ...this.qrCodeSettings, height: formValues.basicHeight }
+        } else { this.codeErrors++ }
+      }      
+    }
+
+    if (formValues.basicColorDark) { // dark color check
+      this.qrCodeSettings = { ...this.qrCodeSettings, colorDark: formValues.basicColorDark }
+    } else {
+      this.qrCodeSettings = { ...this.qrCodeSettings, colorDark: '#000000' }
+    }
+
+    if (formValues.basicColorLight) { // light color check
+      this.qrCodeSettings = { ...this.qrCodeSettings, colorLight: formValues.basicColorLight }
+    } else {
+      this.qrCodeSettings = { ...this.qrCodeSettings, colorLight: '#ffffff' }
+    }
+
+    new QRCode(this.qrcode.nativeElement, this.qrCodeSettings);
   }
 }
