71cc1246cac59f752509cae6731b922f0bcd88ef
zbelicza85 <zoltan.belicza@vertican.com>
2021-03-04 16:52:49 +0100

    mvp beta is finished, basic functionalities and validations added, todo file added

---
+++ TODO.md
@@ -0,0 +1,24 @@
+
+# TODO list for the QR Code generator
+
+## _Current version 0.0.1_
+
+build badge's future place
+
+Qrapp-bs is currently in an MVP state. There could be a lot of bugs, which would need issues to be created on github. However this documentation is only for listing purposes for the work to be done or would be good to have features, developments, additions, etc.
+
+## Work to be done
+
+- check and fix the template-part validation message malfunctions
+- check and fix clear/reset post style issues with form field validation
+- add Husky pre-commit check
+- add project to TravisCI, if working properly, add badge to the top section of this document
+- add proper download functionality as PNG image
+- dynamically generate timestamp at drawing time to show under preview image
+- add advanced options to the create form
+- add proper validation to the advanced parts
+- add proper unit testing with Jest to the project
+- add coverage report after unit testing is properly implemented
+- add multilingual support to the app
+
+With each and every bugfix and new feature added, a new release should be done as path release. Once the MVP is finalized with the correct core functions, a minor version release should be made.
--- src/app/qr-form/qr-form.component.ts
+++ src/app/qr-form/qr-form.component.ts
@@ -12,9 +12,11 @@ export class QrFormComponent implements OnInit, AfterViewInit {
   onlyBasicVisible = true;
   qrCodeSettings = <any>{};
   qrCodeForm!: FormGroup;
-  codeSubmitted = false;
+  submitted = false;
   codeErrors: number = 0;
   showOriginal = false;
+  originalCanvas: any;
+  destinationCanvas: any;
 
 
   constructor(
@@ -24,10 +26,8 @@ export class QrFormComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
     this.qrCodeForm = this.fb.group(
       {
-        basicText: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(256)]],
-        basicWidth: ['256', [Validators.required, Validators.min(0), Validators.max(1024)]],
-        basicHeight: ['256', [Validators.min(0), Validators.max(1024)]],
-        basicEqualSizes: ['', []],
+        basicText: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(256)]],
+        basicWidth: ['', [Validators.required, Validators.min(340), Validators.max(1024)]],
         basicColorDark: ['#000000', [Validators.pattern('^#+([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$')]],
         basicColorLight: ['#ffffff', [Validators.pattern('^#+([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$')]],
         },  
@@ -53,12 +53,6 @@ export class QrFormComponent implements OnInit, AfterViewInit {
   get basicWidth() {
     return this.qrCodeForm.get('basicWidth');
   }
-  get basicHeight() {
-    return this.qrCodeForm.get('basicHeight');
-  }
-  get basicEqualSizes() {
-    return this.qrCodeForm.get('basicEqualSizes');
-  }
   get basicColorDark() {
     return this.qrCodeForm.get('basicColorDark');
   }
@@ -69,35 +63,31 @@ export class QrFormComponent implements OnInit, AfterViewInit {
   resetForm(): void {
     this.showMoreSettings();
     this.qrCodeSettings = {};
+    if (this.destinationCanvas && this.destinationCanvas.hasChildNodes()) {
+      this.destinationCanvas.removeChild(this.destinationCanvas.childNodes[0]);
+    }
   }
 
   qrCodeSubmit() {
-    this.codeSubmitted = true;
+    this.submitted = true;
     let formValues;
     formValues = this.qrCodeForm.getRawValue();
 
+    if (this.qrCodeForm.invalid) {
+      return;
+    }
+
     if (formValues.basicText) { // title check
-      if (formValues.basicText.length > 4 || formValues.basicText.length < 260) {
+      if (formValues.basicText.length >= 4 || formValues.basicText.length <= 260) {
         this.qrCodeSettings = { ...this.qrCodeSettings, text: formValues.basicText }
       } else { this.codeErrors++ }
     }
 
     if (formValues.basicWidth) { // width check
-      if (formValues.basicWidth < 1024 || formValues.basicWidth > 0) {
+      if (formValues.basicWidth <= 1024 || formValues.basicWidth >= 340) {
         this.qrCodeSettings = { ...this.qrCodeSettings, width: formValues.basicWidth }
-      } else { this.codeErrors++ }
-    }
-
-    if (formValues.basicEqualSizes) { // height and size check
-      if (formValues.basicWidth < 1024 || formValues.basicWidth > 0) {
         this.qrCodeSettings = { ...this.qrCodeSettings, height: formValues.basicWidth }
       } else { this.codeErrors++ }
-    } else {
-      if (formValues.basicHeight) {
-        if (formValues.basicHeight < 1024 || formValues.basicHeight > 0) {
-          this.qrCodeSettings = { ...this.qrCodeSettings, height: formValues.basicHeight }
-        } else { this.codeErrors++ }
-      }      
     }
 
     if (formValues.basicColorDark) { // dark color check
@@ -112,6 +102,58 @@ export class QrFormComponent implements OnInit, AfterViewInit {
       this.qrCodeSettings = { ...this.qrCodeSettings, colorLight: '#ffffff' }
     }
 
-    if (this.codeErrors < 1) new QRCode(this.qrcode.nativeElement, this.qrCodeSettings);
+    this.createCode();
+  }
+
+  createCode() {
+    if (this.codeErrors == 0) {
+      const canvasIsFine = this.checkCanvas();
+      if (canvasIsFine) {
+        this.drawCode(this.qrCodeSettings);
+      }
+    }
+  }
+
+  checkCanvas() {
+    const originalContent = document.querySelectorAll('canvas');
+    if (originalContent.length > 0) {
+      originalContent[0].remove();
+      return true;
+    } else {
+      return true;
+    }
+  }
+
+  drawCode(options: any) {
+    new QRCode(this.qrcode.nativeElement, options);
+    if (document.querySelector('canvas')) {
+      this.originalCanvas = document.querySelector('canvas');
+      this.checkDimensions(this.originalCanvas);
+    }
+  }
+
+  checkDimensions(canvas: any) {
+    const width = canvas.width;
+    const height = canvas.height;
+    if (width === height) {
+      this.cloneCanvas(document.getElementById('previewCanvas'));
+    } else {
+      canvas.height = canvas.width;
+      this.cloneCanvas(document.getElementById('previewCanvas'));
+    }
+  }
+
+  cloneCanvas(identifier: any) {
+    this.destinationCanvas = identifier;
+    const destination = this.destinationCanvas;
+    if (destination.hasChildNodes()) {
+      destination.removeChild(destination.childNodes[0]);
+    }
+    let image = new Image();
+    image.className = 'img-fluid';
+    image.src = this.originalCanvas.toDataURL();
+    if (destination) {
+      destination.appendChild(image);
+    }
   }
 }
